d388ea41eea7bf3368fc6a670f23938dae7ceea1
Jinyshin <iwbm312@gmail.com>
2025-03-25 10:16:15 +0900

    feat: error handling for Future<?> object

--- src/main/java/com/example/concurrencypractice/ConcurrencyPracticeApplication.java
+++ src/main/java/com/example/concurrencypractice/ConcurrencyPracticeApplication.java
@@ -1,14 +1,19 @@
 package com.example.concurrencypractice;
 
 import com.example.concurrencypractice.lock.redisson.LockTestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+@Slf4j
 @SpringBootApplication
 public class ConcurrencyPracticeApplication {
 
@@ -16,23 +21,28 @@ public class ConcurrencyPracticeApplication {
         SpringApplication.run(ConcurrencyPracticeApplication.class, args);
     }
 
+
     @Bean
     CommandLineRunner run(LockTestService lockTestService) {
         return args -> {
             String clientName = System.getProperty("client.name", "JVM-Default");
-            boolean useLock = Boolean.parseBoolean(System.getProperty("use.lock", "true")); // 락 사용 여부
-            int threadCount = 4; // 각 JVM 마다 4개의 스레드
-
-            lockTestService.incrementJvmCount();
+            int threadCount = 6;
 
             try (ExecutorService executor = Executors.newFixedThreadPool(4)) {
+                List<Future<?>> futures = new ArrayList<>();
                 for (int i = 1; i <= threadCount; i++) {
                     String threadName = "Thread-" + i;
-                    executor.submit(() -> lockTestService.accessSharedResource(clientName, threadName, useLock));
+                    futures.add(executor.submit(() -> lockTestService.accessSharedResource(clientName, threadName)));
+                }
+                for (Future<?> future : futures) {
+                    try {
+                        future.get();
+                    } catch (Exception e) {
+                        log.error("스레드 실행 중 오류 발생: ", e);
+                    }
                 }
             }
             System.out.println("✅ [" + clientName + "] 최종 공유 자원 값: " + lockTestService.getSharedResourceValue());
-            lockTestService.decrementJvmCountAndCleanup();
         };
     }
 }
